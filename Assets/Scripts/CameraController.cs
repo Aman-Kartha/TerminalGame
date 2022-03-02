5eb93c4e07e1630d8115284eb4105a0e57c9beed
Aman Kartha <ak3758@drexel.edu>
2022-03-01 20:17:57 -0500

    first

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] views;
+    [SerializeField]
+    float transitionSpeed;
+
+    Transform currentView;
+    bool check = true;
+
+    int counter = 0;
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+            currentView = views[0];
+            StartCoroutine(ExecuteAfterTime(2, views[1]));;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentView = views[2];
+            StartCoroutine(ExecuteAfterTime(2, views[3])); ;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentView = views[4];
+            StartCoroutine(ExecuteAfterTime(2, views[5])); ;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentView = views[6];
+            StartCoroutine(ExecuteAfterTime(2, views[7])); ;
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (currentView != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+
+            Vector3 currentAngle = new Vector3(
+             Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+             Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+             Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+            transform.eulerAngles = currentAngle;
+        }
+    }
+
+
+    public void loop()
+    {
+        if (check && counter == 0)
+        {
+
+            currentView = views[0];
+            StartCoroutine(ExecuteAfterTime(1, views[1]));
+            counter++;
+        }
+        if (check && counter == 1)
+        {
+            currentView = views[2];
+            StartCoroutine(ExecuteAfterTime(1, views[3]));
+            counter++;
+        }
+        if (check && counter == 2)
+        {
+            currentView = views[4];
+            StartCoroutine(ExecuteAfterTime(1, views[5]));
+            counter++;
+        }
+        if (check && counter == 3)
+        {
+            currentView = views[6];
+            StartCoroutine(ExecuteAfterTime(1, views[7]));
+
+            counter = 0;
+        }
+    }
+    IEnumerator ExecuteAfterTime(float time,Transform vector)
+    {
+        check = false;
+        yield return new WaitForSeconds(time);
+        currentView = vector;
+        check = true;
+
+    }
+}
---
+++ Assets/Scripts/collider.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class collider : MonoBehaviour
+{
+    int count = 0;
+    [SerializeField]
+    GameObject[] arr;
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject cam;
+    [SerializeField]
+    GameObject list;
+    [SerializeField]
+    GameObject icon;
+    [SerializeField]
+    bool left;
+   
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("food"))
+        {
+            if (GameObject.Find("Engine").GetComponent<engineScript>().chosen != null)
+            {
+                if (!String.Equals(other.gameObject.name, GameObject.Find("Engine").GetComponent<engineScript>().chosen.gameObject.name)
+                    && !String.Equals(other.gameObject.name, GameObject.Find("Engine").GetComponent<engineScript>().chosen1.gameObject.name)
+                    && !String.Equals(other.gameObject.name, GameObject.Find("Engine").GetComponent<engineScript>().chosen2.gameObject.name ))
+                {
+                    GameObject.Find("Engine").GetComponent<engineScript>().Health -= 1;
+
+                }
+            }
+            Destroy(other.gameObject);      
+        }
+
+        if (other.gameObject.CompareTag("next"))
+        {
+            cam.GetComponent<CameraController>().loop();
+        }
+
+     
+        /*
+        if (other.gameObject.CompareTag("food"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log(GameObject.Find("Engine").GetComponent<Engine>().oneN );
+            Debug.Log(GameObject.Find("Engine").GetComponent<Engine>().twoN);
+            Debug.Log(GameObject.Find("Engine").GetComponent<Engine>().threeN);
+            Debug.Log(other.gameObject.name);
+            if (!(other.gameObject.name.Equals( GameObject.Find("Engine").GetComponent<Engine>().oneN)||
+               other.gameObject.name.Equals( GameObject.Find("Engine").GetComponent<Engine>().twoN)||
+               other.gameObject.name.Equals( GameObject.Find("Engine").GetComponent<Engine>().threeN)))
+            {
+                arr[count].SetActive(true);
+                count = count + 1;
+            }
+        }*/
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("test") && left)
+        {
+            list.SetActive(true);
+            icon.SetActive(false);
+        }
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("test") && left)
+        {
+            list.SetActive(false);
+            icon.SetActive(true);
+        }
+    }
+
+
+
+}
